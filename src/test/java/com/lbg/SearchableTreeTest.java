adc1ffa4a5cb8127d50b9d6def12d34ee9229e01
Adam.B3 <readingfootyclub1871@gmail.com>
2023-10-23 14:48:26 +0100

    small changes

--- src/test/java/com/lbg/SearchableTreeTest.java
+++ src/test/java/com/lbg/SearchableTreeTest.java
@@ -8,16 +8,43 @@ class SearchableTreeTest {
 
     @Test
     void populateContainer() {
+        SearchableTree cut = new SearchableTree();
+        String expected_value = "selvyn";
+//        int test = 22/0;
+        String actual_value = cut.populateContainer();
+        assertEquals(expected_value,actual_value);
 
     }
 
     @Test
-    void find() {
-
+    void findWhenItemThere() {
+        SearchableTree cut = new SearchableTree();
+        cut.populateContainer();
+        int expected_value = 0;
+        int actual_value = cut.find("selvyn");
+        assertEquals(expected_value,actual_value);
     }
 
     @Test
-    void findIfLastItem() {
+    void findItemWhenNotThere(){
+        SearchableTree cut = new SearchableTree();
+        cut.populateContainer();
+        int expected_value = -1;
+        int actual_value = cut.find("Adam");
+        assertEquals(expected_value,actual_value);
+    }
+
+    @Test
+    void findIfLastItemWhenItemThere() {
+        SearchableTree cut = new SearchableTree();
+        cut.populateContainer();
+        boolean expected_value = true;
+        boolean actual_value = cut.findIfLastItem("taxes");
+        assertEquals(expected_value,actual_value);
+    }
+
+    @Test
+    void findIfLastItemWhenItemNotThere() {
 
     }
 
